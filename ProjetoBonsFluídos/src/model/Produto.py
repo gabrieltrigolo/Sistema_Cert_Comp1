dea52e322508bf025967f01002c482d9a4cd3e48
Mateus Bernardi Alves <mateusbnalves@gmail.com>
2025-01-07 10:15:27 -0300

    Update Produto.py

--- ProjetoBonsFluídos/src/model/Produto.py
+++ ProjetoBonsFluídos/src/model/Produto.py
@@ -1,37 +1,72 @@
 class Produto:
-    def __init__(self, nome, categoria, quantidade, validade, lote):
-        self.__nome = nome
-        self.__categoria = categoria
-        self.__quantidade = quantidade
-        self.__validade = validade
-        self.__lote = lote
+    def __init__(self, produto_id=None, nome=None, preco_unitario=0.0, quantidade=0, categoria=None, preco_custo=0.0):
+        
+        self._produto_id = produto_id
+        self._nome = nome
+        self._preco_unitario = preco_unitario
+        self._quantidade = quantidade
+        self._categoria = categoria
+        self._preco_custo = preco_custo
 
-    def getNome(self):
-        return self.__nome
+    @property
+    def produto_id(self):
+        return self._produto_id
 
-    def setNome(self, nome):
-        self.__nome = nome
+    @produto_id.setter
+    def produto_id(self, value):
+        self._produto_id = value
 
-    def getCategoria(self):
-        return self.__categoria
+    @property
+    def nome(self):
+        return self._nome
 
-    def setCategoria(self, categoria):
-        self.__categoria = categoria
+    @nome.setter
+    def nome(self, value):
+        self._nome = value
 
-    def getQuantidade(self):
-        return self.__quantidade
+    @property
+    def preco_unitario(self):
+        return self._preco_unitario
 
-    def setQuantidade(self, quantidade):
-        self.__quantidade = quantidade
+    @preco_unitario.setter
+    def preco_unitario(self, value):
+        self._preco_unitario = value
 
-    def getValidade(self):
-        return self.__validade
+    @property
+    def quantidade(self):
+        return self._quantidade
 
-    def setValidade(self, validade):
-        self.__validade = validade
+    @quantidade.setter
+    def quantidade(self, value):
+        self._quantidade = value
 
-    def getLote(self):
-        return self.__lote
+    @property
+    def categoria(self):
+        return self._categoria
 
-    def setLote(self, lote):
-        self.__lote = lote
+    @categoria.setter
+    def categoria(self, value):
+        self._categoria = value
+
+    @property
+    def preco_custo(self):
+        return self._preco_custo
+
+    @preco_custo.setter
+    def preco_custo(self, value):
+        self._preco_custo = value
+
+    def __eq__(self, other):
+        if isinstance(other, Produto):
+            return self.produto_id == other.produto_id
+        return False
+
+    def __hash__(self):
+        return hash(self.produto_id)
+
+    def __repr__(self):
+        return (
+            f"Produto(produto_id={self.produto_id}, nome='{self.nome}', "
+            f"preco_unitario={self.preco_unitario:.2f}, quantidade={self.quantidade}, "
+            f"categoria='{self.categoria}', preco_custo={self.preco_custo:.2f})"
+        )
